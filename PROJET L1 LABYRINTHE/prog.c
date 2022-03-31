99c982cf8c7263f3a26990fc23c25e5d497e0f41
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-03-31 08:51:47 +0200

    Update prog.c

--- PROJET L1 LABYRINTHE/prog.c
+++ PROJET L1 LABYRINTHE/prog.c
@@ -19,12 +19,13 @@ typedef struct case_tab cstb;
 void matrice(int cote, cstb **p){
 	int cpt=0,i;
 	cstb **tab;
-	tab=(cstb**)malloc(sizeof(cstb*)*cote);
+	tab=(cstb**)malloc(sizeof(cstb*)*cote); //crée un tableau 1D de taille cote
+	
 	for(i=0;i<cote;i++){
-		tab[i]=(cstb*) malloc(sizeof(cstb)*cote);
+		tab[i]=(cstb*) malloc(sizeof(cstb)*cote); //crée la 2e D de taille cote dans tableau
 	}
 
-	for(i=0;i<cote;i++){
+	for(i=0;i<cote;i++){ //affecte des valeurs a chaque case du tableau de type case_tab (cstb)
 		for(int j=0;j<cote;j++){
 			tab[i][j].nb_case=cpt,tab[i][j].Haut=1,tab[i][j].Bas=1,tab[i][j].Gauche=1,tab[i][j].Droite=1,tab[i][j].pass=0;
 			cpt++;
@@ -32,28 +33,31 @@ void matrice(int cote, cstb **p){
 	}
 	p=tab;
 	
-	for(int i=0;i<cote;i++){
+	for(int i=0;i<cote;i++){ //affiche la matrice / tableau
 		printf("\n\n");
 		for(int j=0;j<cote;j++){
-			printf("|\t%d \t|",tab[i][j].nb_case);
+			printf("|\t%d \t|",p[i][j].nb_case);
 		}
 	} 
 }
 	
 
 int main(){
+	
 	cstb** maze=NULL;
 	int cote=5;
+	
+	
 	matrice(cote,maze);
 	
-	if(maze==NULL){
-		printf("ECHEC ALLOCATION");
+	if(maze==NULL){ 
+		printf("\n\n\t\t\t ----- ECHEC ALLOCATION ----");
 		return 0;
 	}else{
-		printf("ALLOCATION REUSSIÉ");
+		printf("ALLOCATION REUSSIÉ!!!!!!!");
 	}
 	
-	/*SEGMENTATION FAULT ICI -> Probleme avec le maze[][].nb_case ??  ====> echec d'allocation, revoir la fonction !!!!!
+	/*SEGMENTATION FAULT ICI -> Probleme avec le maze[][].nb_case ??  ====> echec d'allocation, revoir la fonction qui ne l'initialise pas le tableau dans la variable maze!
 	for(int i=0;i<cote;i++){
 		printf("\n");
 		for(int j=0;j<cote;j++){
