35ce4df328ed12d12ef1700073b86501542283f4
Tokaa7 <ksoares@outlook.fr>
2022-04-05 00:22:09 +0200

    Update Exo3

--- TP1/Exo3.c
+++ TP1/Exo3.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
+int min2int(int a, int b){
+	if(a<b)return a;
+	else return b;
+}
+
+int min3int(int a, int b, int c){
+	return min2int(min2int(a,b),min2int(b,c));
+}
+
+int min4int(int a,int b,int c,int d){
+	return min3int(min2int(a,b),min2int(b,c),min2int(c,d));
+}
+
 int main(){
 	
+	int a=36;
+	int b=48;
+	int c=17;
+	int d=5;
 	
+	printf("\n\nMinimum entre %d et %d : %d",a,b,min2int(a,b));
 	
+	printf("\n\nMinimum entre %d, %d et %d : %d",a,b,c,min3int(a,b,c));
+
+	printf("\n\nMinimum entre %d, %d, %d et %d : %d",a,b,c,d,min4int(a,b,c,d));
+
 	return 0;
 }
 
