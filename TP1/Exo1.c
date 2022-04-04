c0ede54fa9327ea16c2d88d7bbee1935d5c948f8
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-04-04 23:36:49 +0200

    Create Exo1.c

---
+++ TP1/Exo1.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <math.h>
+
+int test_premier(long long nb){
+	
+	for(long i=2;i<sqrt(nb);i++){
+		if(nb%i==0){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+long long pp_facteur_prem(long long nb){
+	long long i,pp_div=0;
+	
+	for(i=sqrt(nb);i>1;i--){
+		if(test_premier(i)==1){
+			if(nb%i==0){
+				pp_div=i;
+				nb/=i;
+			}
+		}
+	}
+	if(pp_div!=0) return pp_div;
+	else return nb;
+}
+
+long long pg_facteur_prem(long long nb){
+	long long i;
+	
+	for(i=sqrt(nb);i>1;i--){
+		if(test_premier(i)==1){
+			if(nb%i==0){
+				return i;
+			}
+		}
+	}
+	return nb;
+}
+
+int test_nbprefect(long long nb){
+	long long sum=0;
+	
+	for(long long i=nb/2;i>0;i--){
+		if(nb%i==0){
+			sum+=i;
+		}
+	}
+	if(sum==nb) return 1;
+	else return 0;
+}
+
+long test_Harshad(long long nb){
+	if(nb<1)return 0;
+	long long temp=nb;
+	long sum=0;
+	
+	while(temp!=0){
+		sum+=(temp%10);
+		temp/=10;
+	}
+	if(nb%sum==0) return 1;
+	else return 0;
+}
+
+int main(){
+	
+	long long a=600851475143;
+	long long b=8128;
+	long long c=195;
+	printf("\nPremier ou non : %d",test_premier(a));
+	printf("\nPlus petit facteur premier : %lld",pp_facteur_prem(a));
+	printf("\nPlus grand facteur premier : %lld",pg_facteur_prem(a));
+	
+	printf("\nParfait ou non : %d",test_nbprefect(b));
+	
+	printf("\nTest Harshad : %ld",test_Harshad(c));
+	
+	return 0;
+}
