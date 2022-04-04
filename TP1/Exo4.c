72149f7c49fedfb709e31a7fe64d43b6fd2159b6
Tokaa7 <ksoares@outlook.fr>
2022-04-05 00:44:28 +0200

    Added Exo4.c

---
+++ TP1/Exo4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int puissance(int x, int n){
+	if(n==0) return 1;
+	int aux=x;
+	for(int i=1;i<n;i++){
+		x*=aux;
+	}
+	return x;
+}
+
+int nb_chiffres(int x){
+	int cpt=0;
+	while(x!=0){
+		x/=10;
+		cpt++;
+	}
+	return cpt;
+}	
+
+int narcissique(int nb){
+	int nbc=nb_chiffres(nb),sum=0,aux=nb;
+	
+	while(aux!=0){
+		sum+=puissance((aux%10),nbc);
+		aux/=10;
+	}
+	if(sum==nb) return 1;
+	else return 0;
+}
+
+int main(){
+	
+	int a=3,b=15,c=548834,d=93084;
+	
+	printf("\n%d^%d = %d",a,b,puissance(a,b));
+	
+	printf("\n\nNombre de chiffres de %d^%d : %d",a,b,nb_chiffres(puissance(a,b)));
+
+	printf("\n\n%d narcissique ? : %d",c,narcissique(c));
+	
+	printf("\n\n%d narcissique ? : %d",d,narcissique(d));
+
+	return 0;
+}
+
