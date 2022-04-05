df0c3d79eadcc054160c49b60f5a6a19ad4136fa
Tokaa7 <ksoares@outlook.fr>
2022-04-05 19:22:23 +0200

    Update TD4

--- TD4/Pages-2-a-4.c
+++ TD4/Pages-2-a-4.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int* allocation1(int taille){
+	int *tab=malloc(sizeof(int)*taille);
+	return tab;
+}
+
+int*allocation2(int taille){
+	int *tab=calloc(sizeof(int),taille);
+	return tab;
+}
+
+int* allocation3(int taille,int n){
+	srand(time(NULL));
+	int *tab=malloc(sizeof(int)*taille);
+	
+	for(int i=0;i<taille;i++){
+		*(tab+i)=rand()%n;
+	}
+	return tab;
+}
+
+void liberation1(int *tab){
+	free(tab);
+}
 
 int main(){
   
-  //A FAIRE
+  printf("\n%p\n\n",allocation1(3));
+  int*tab2=allocation2(3);
+  int*tab3=allocation3(6,8);
   
+  for(int i=0;i<3;i++){
+	printf("%d ",*(tab2+i));
+	}
+	printf("\n\ntab3 avant liberation :\n");
+	for(int i=0;i<3;i++){
+	printf("%d ",*(tab3+i));
+	}
+  liberation1(tab2);
+  liberation1(tab3);
+  
+  printf("\n\ntab3 apres liberation :\n");
+	for(int i=0;i<3;i++){
+	printf("%d ",*(tab3+i));
+	}
   return 0;
 }
---
+++ TD4/pages-5-a-8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	
+	//A FAIRE
+	
+	return 0;
+}
+
