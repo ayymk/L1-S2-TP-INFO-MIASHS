79fb587f524da4a0ed47e4f42ba55d37bacf29c0
Tokaa7 <ksoares@outlook.fr>
2022-04-06 16:41:04 +0200

    Added chiffrement de Cesar

--- TD5/chiffrement_cesar.c
+++ TD5/chiffrement_cesar.c
@@ -3,19 +3,22 @@
 
 void cesar(char *source, char *destination, int decalage){
 	FILE *fp_s=NULL, *fp_d=NULL;
-	//fp_s = fopen(source,"r");
-	if(fp_s==NULL)perror("Echec ouverture fichier source");
-	fp_d = fopen(destination,"w");
-	if(fp_d==NULL)perror("Echec ouverture fichier destination");
-	
-	printf("Fin de fonction");
-	
-	
+	fp_s = fopen(source,"r"), fp_d = fopen(destination,"w");
+	if(fp_s==NULL || fp_d==NULL)perror("Echec lors de l'ouverture d'un des fichier");
+
+	while(1){
+		char cchar=fgetc(fp_s);
+		if(cchar==EOF)break;
+		if(cchar==' ')fprintf(fp_d," ");
+		else{
+			fprintf(fp_d,"%c",cchar+decalage);
+		}	
+	}
 }
 
 int main(){
 	
-	cesar("c_source.txt","c_destination.txt",4);
+	cesar("c_source.txt","c_destination.txt",5);
 	
 	return 0;
 }
