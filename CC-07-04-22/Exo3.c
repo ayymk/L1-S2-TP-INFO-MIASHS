082eb0c09ff39320b829b51625861cdb489d4ad8
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-04-07 20:41:24 +0200

    Update Exo3.c

--- CC-07-04-22/Exo3.c
+++ CC-07-04-22/Exo3.c
@@ -12,32 +12,31 @@ int* allocation(int taille){
 		perror("\nValeur de taille impaire necessaire");
 		exit(EXIT_FAILURE);
 	}
-	
-	int*tab=malloc(sizeof(int)*taille);
+	int*tab = malloc(sizeof(int)*taille);
 	int i, nb, aux, c1, c2;
 	
-	for(i=0;i<taille;i++){
-		if(i%2==0)nb=rand();
-		tab[i]=nb;
+	for(i = 0; i < taille; i++){
+		if(i%2==0)nb = rand();
+		tab[i] = nb;
 	}
-	for(i=0;i<(taille*3);i++){
-		c1=rand()%taille,c2=rand()%taille;
-		aux=tab[c1];
-		tab[c1]=tab[c2];
-		tab[c2]=aux;
+	for(i=0 ; i < (taille*3); i++){
+		c1 = rand()%taille,c2 = rand()%taille;
+		aux = tab[c1];
+		tab[c1] = tab[c2];
+		tab[c2] = aux;
 	}
 	return tab;
 }
 	
 void single(int *tab,int taille){ 
-	int i,j=0,cpt,nb;
+	int i, j = 0, cpt, nb;
 	do{
-		cpt=0,nb=tab[j];
-		for(i=0;i<taille;i++){
+		cpt = 0, nb = tab[j];
+		for(i = 0; i < taille; i++){
 			if(tab[i]==nb)cpt++;
 		}
 		j++;
-	}while(cpt==2 && j<(taille-1));
+	}while(cpt==2 && j < (taille-1));
 	printf("\ncpt = %d et j = %d",cpt,j);
 	if(cpt==1)printf("\nLa valeur présente une seule fois dans le tableau est %d\n",nb);
 	else printf("\nAucune des valeurs du tableau n'es présente qu'une seule fois\n");
@@ -45,9 +44,9 @@ void single(int *tab,int taille){
 
 int main(){
   
-  int *tableau=allocation(SIZE);
+  int *tableau = allocation(SIZE);
   
-  for(int i=0;i<SIZE;i++){
+  for(int i = 0; i < SIZE; i++){
 	printf("\n%d",tableau[i]);
   }
   
