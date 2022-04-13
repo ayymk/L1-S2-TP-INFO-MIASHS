fad451a616e606f4bd91cd7aa7662fa086b60df1
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-04-14 00:52:04 +0200

    Update Pages-07-a-09.c

--- TD6 - Récursivité/Pages-07-a-09.c
+++ TD6 - Récursivité/Pages-07-a-09.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 void tous_les_entiers_de_x_a_y_recursif( int debut , int fin){
-	if(debut<fin){
+	if(debut < fin){
 		printf("%d\n",debut);
-		tous_les_entiers_de_x_a_y_recursif(debut+1,fin);
+		tous_les_entiers_de_x_a_y_recursif(debut + 1, fin);
 	}else printf("%d\n",fin);
 }
 
 int nombre_de_chiffres_recursif (int n){
-	if(n<10) return 1;
-	return 1+nombre_de_chiffres_recursif(n/10);	
+	if(n < 10) return 1;
+	return 1 + nombre_de_chiffres_recursif(n / 10);	
 }
 
 int pgcd(int a, int b){
 	if(b==0)return a;
-	else return pgcd(b,a%b);
+	else return pgcd(b, a % b);
 }
 
 int main(){
 	
-	int x=28,y=40,nb=1293,a=100,b=180;
+	int x = 28, y = 40, nb = 1293, a = 100, b = 180;
 	
 	tous_les_entiers_de_x_a_y_recursif(x,y);
 	
-	printf("\nNombre de chiffre de %d : %d\n",nb,nombre_de_chiffres_recursif(nb));
+	printf("\nNombre de chiffre de %d : %d\n",nb, nombre_de_chiffres_recursif(nb));
 	
-	printf("\nPGCD(%d,%d) = %d",a,b,pgcd(a,b));
+	printf("\nPGCD(%d,%d) = %d",a, b, pgcd(a,b));
 	
 	return 0;
 }
