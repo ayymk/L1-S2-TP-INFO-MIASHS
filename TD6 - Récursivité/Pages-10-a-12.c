915058855f415e3c3fd4ad110f8018e876559a66
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-04-14 00:54:51 +0200

    Update Pages-10-a-12.c

--- TD6 - Récursivité/Pages-10-a-12.c
+++ TD6 - Récursivité/Pages-10-a-12.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 
 int fibonacci_recursif(int n){
-	if(n<0)return n;
+	if(n < 0)return n;
 	if(n==0)return 0;
 	if(n==1)return 1;
-	return fibonacci_recursif(n-1)+fibonacci_recursif(n-2);
+	return fibonacci_recursif(n - 1) + fibonacci_recursif(n - 2);
 }
 
 int autre_suite(int n){
-	if(n<0)return n;
+	if(n < 0)return n;
 	if(n==0) return 3;
 	if(n==1) return 5;
-	int u=autre_suite(n-1);
-	return u*u*autre_suite(n-2);
+	int u = autre_suite(n - 1);
+	return u * u * autre_suite(n - 2);
 	
 }
 
 int encore_suite(int n){
-	if(n<0)return n;
+	if(n < 0)return n;
 	if(n<3)return 1;
 	if(n%2==0)return 1;
-	return 2*encore_suite(n-2);
+	return 2 * encore_suite(n - 2);
 	
 }
 
 int main(){
 	
-	int fibo=8,n=3,n2=15;
+	int fibo = 8, n = 3, n2 = 15;
 	
-	printf("\n%de terme de Fibo = %d",fibo,fibonacci_recursif(fibo));
+	printf("\n%de terme de Fibo = %d",fibo, fibonacci_recursif(fibo));
 	
-	printf("\n\n%de terme de autre_suite = %d",n,autre_suite(n));
+	printf("\n\n%de terme de autre_suite = %d",n, autre_suite(n));
 	
-	printf("\n\n%de terme de encore_suite = %d",n2,encore_suite(n2));
+	printf("\n\n%de terme de encore_suite = %d",n2, encore_suite(n2));
 	
 	return 0;
 }
