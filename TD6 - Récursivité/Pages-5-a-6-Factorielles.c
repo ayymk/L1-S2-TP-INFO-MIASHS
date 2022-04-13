9381ea263a03cd23e052227731e441ad27b0a6be
Tokaa7 <ksoares@outlook.fr>
2022-04-13 23:22:27 +0200

    Updated TD6

---
+++ TD6 - Récursivité/Pages-5-a-6-Factorielles.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int factorielle_iterative(int n){
+	int nb=1;
+	for(int i=2;i<=n;i++){
+		nb*=i;
+	}
+	return nb;
+}
+
+int factorielle_recursive(int n){
+	if(n<2)return 1;
+	return n*factorielle_recursive(n-1);
+}
+
+int main(){
+	
+	int fact=4;
+	
+	printf("\nItérative : %d! = %d\n",fact,factorielle_iterative(fact));
+
+	printf("\nRécursive : %d! = %d\n",fact,factorielle_recursive(fact));
+	
+	return 0;
+}
+
--- TD6 - Récursivité/sans titre.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	
-	// A FAIRE
-	
-	return 0;
-}
-
