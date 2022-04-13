9e012350eab56b591471dd4ac455e1354124f90e
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-04-14 00:53:23 +0200

    Update Pages-02-a-04.c

--- TD6 - Récursivité/Pages-02-a-04.c
+++ TD6 - Récursivité/Pages-02-a-04.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-int puissance(int x,int n){
-	if(n<0) return n;
+int puissance(int x, int n){
+	if(n < 0) return n;
 	if(n==0)return 1;
-	int nb=x,i;
+	int nb = x, i;
 	for(i = 1; i < n; i++){
-		nb *= x;
+		nb*= x;
 	}
 	return nb;
 }
 
 int fonction_puissance_recursive(int x, int n){
 	if(n==0)return 1;
-	return x * fonction_puissance_recursive(x,n-1);
+	return x * fonction_puissance_recursive(x, n - 1);
 }
 
 int fonction_puissance_recursive_rapide(int x, int n){ // pas compris comment la faire avec un seul appel recursif
 	if(n==0)return 1;
-	if(n%2==1)return x * fonction_puissance_recursive_rapide(x*x,(n-1)/2);
-	else return fonction_puissance_recursive_rapide(x*x,n/2);
+	if(n%2==1)return x * fonction_puissance_recursive_rapide(x*x,(n - 1)/2);
+	else return fonction_puissance_recursive_rapide(x*x, n/2);
 	
 }
 
@@ -26,11 +26,11 @@ int main(){
 	
 	int nb = 3, pui = 4;
 	
-	printf("\nBasique : %d^%d = %d\n",nb,pui,puissance(nb,pui));
+	printf("\nBasique : %d^%d = %d\n",nb, pui, puissance(nb, pui));
 
-	printf("\nRecursive : %d^%d = %d\n",nb,pui,fonction_puissance_recursive(nb,pui));
+	printf("\nRecursive : %d^%d = %d\n",nb, pui, fonction_puissance_recursive(nb, pui));
 	
-	printf("\nRecursive rapide : %d^%d = %d\n",nb,pui,fonction_puissance_recursive_rapide(nb,pui));
+	printf("\nRecursive rapide : %d^%d = %d\n",nb, pui, fonction_puissance_recursive_rapide(nb, pui));
 
 		
 	return 0;
