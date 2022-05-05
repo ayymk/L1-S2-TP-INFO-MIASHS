d097cfbdc739826ec5742b11a0b45d7f84a2d5e3
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-05-06 00:28:50 +0200

    Update Pages-13-a-16.c

--- TD6 - Récursivité/Pages-13-a-16.c
+++ TD6 - Récursivité/Pages-13-a-16.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-int dichotomie recursive(int *tab, int debut, int fin, int valeur){
+int dichotomie_recursive(int *tab, int debut, int fin, int valeur){
 	
+	if(debut==fin)return -1;
+	m=(debut+fin)/2;
+
+	if(tab[m]==valeur) return m;
 	
+	if(tab[m]>valeur)return(dichotomie_recursive(tab,debut,m-1,valeur);
+				
+	else return(dichotomie_recursive(tab,m+1,fin,valeur);
 }
 
 
 
 int main(){
 	
-	//A FAIRE
+	int taille=10,tab[taille]={1,4,7,10,13,23,158,239,1236,3573}
 	
+	printf("\n%d",dichotomie_recursive(tab,0,taille-1,4));
+	printf("\n%d",dichotomie_recursive(tab,0,taille-1,1236));
+	printf("\n%d",dichotomie_recursive(tab,0,taille-1,32748));
 	return 0;
 }
