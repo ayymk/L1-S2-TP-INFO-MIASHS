e7ac1bf4636b3852e74f02ffe9ad385b71d6b849
Tokaa7 <99510448+Tokaa7@users.noreply.github.com>
2022-09-19 12:27:01 +0200

    Update Exo1.c

--- CC-07-04-22/Exo1.c
+++ CC-07-04-22/Exo1.c
@@ -9,14 +9,14 @@ typedef struct spectacle{
 	char titre;
 }spectacle;
 
-spectacle init_spectable(int j, int m, int a, int s,float p, char t){
+spectacle init_spectacle(int j, int m, int a, int s,float p, char t){
 	spectacle var={j,m,a,s,p,t};
 	return var;
 }
 
 int main(){
 	
-	spectacle var=init_spectable(7,4,2022,2,7.89,'A');
+	spectacle var=init_spectacle(7,4,2022,2,7.89,'A');
 	
 	printf("\nDate : %d/%d/%d \nSalle : %d\nPrix : %f\nPremiere lettre film : %c",var.jour,var.mois,var.annee,var.salle,var.prix,var.titre);
 	
